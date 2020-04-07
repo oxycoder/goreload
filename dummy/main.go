7999d4e74a452a181dc077e1118cfab50358222a
Tung Nguyen <oxycoder@gmail.com>
2020-04-07 15:52:50 +0700

    small hack to let process running after attach dlv

--- dummy/main.go
+++ dummy/main.go
@@ -7,7 +7,19 @@ import (
 
 func main() {
 	fmt.Println("Hello world, I will be changed")
-	time.AfterFunc(3000, func() {
-		fmt.Println("Yah")
+	ch := make(chan int)
+	time.AfterFunc(5*time.Second, func() {
+		fmt.Println("5 seconds passed")
+		ch <- 10
 	})
+	for {
+		select {
+		case i := <-ch:
+			fmt.Println(i, " is coming, end.")
+			return
+		default:
+			fmt.Println("wait")
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
